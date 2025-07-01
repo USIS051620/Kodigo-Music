252dd39a19995b781d6532d62123301beacf6cbd
USIS051620 <marvinjosuesolorzano@gmail.com>
2025-06-30 22:08:13 -0600

    Actualizacion del modo responsivo

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,13 +8,17 @@ const categories = [
   { title: 'Música Nueva', color: '#1E3264', imageUrl: 'https://picsum.photos/seed/p2/200' },
   { title: 'Charts', color: '#8D67AB', imageUrl: 'https://picsum.photos/seed/p3/200' },
   { title: 'Conciertos', color: '#E8115B', imageUrl: 'https://picsum.photos/seed/p4/200' },
-  { title: 'Pop', color: '#148A08', imageUrl: 'https://picsum.photos/seed/p5/200' },
-  { title: 'Rock', color: '#B49BC8', imageUrl: 'https://picsum.photos/seed/p6/200' }
+  { title: 'Música Clásica', color: '#11b9e8', imageUrl: 'https://picsum.photos/seed/p5/200' },
+  { title: 'Pop', color: '#148A08', imageUrl: 'https://picsum.photos/seed/p6/200' },
+  { title: 'Rock', color: '#B49BC8', imageUrl: 'https://picsum.photos/seed/p7/200' },
+  { title: 'Electrónica', color: '#440974', imageUrl: 'https://picsum.photos/seed/p8/200' },
+  { title: 'Música Antigua', color: '#d0a61d', imageUrl: 'https://picsum.photos/seed/p9/200' },
+  { title: 'Reggaeton', color: '#0931a6', imageUrl: 'https://picsum.photos/seed/p10/200' }
 ];
 
 const Search = () => {
   return (
-    <div>
+    <div className="search-container">
       <h1 style={{ fontSize: '28px' }}>Buscar</h1>
       <input
         type="text"
